a507559f6c42db1b6515f1c641324c82d2124732
Vikkans <Jakobsenviktor6@gmail.com>
2023-10-24 18:15:11 +0200

    Add project files.

---
+++ ConsoleApp37/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Consoleapp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            
+
+            bool sign1Exists = false;
+            bool sign2Exists = false;
+
+            Console.WriteLine("Skriv in en sträng:");
+            string userString = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Skriv två tecken:");
+            string signs = Console.ReadLine().ToLower();
+
+            char sign1 = signs[0];
+            char sign2 = signs[1];
+
+            for (int i = 0; i < userString.Length; i++)
+            {
+                if (userString[i] == sign1)
+                {
+                    sign1Exists = true;
+                }
+                else if (userString[i] == sign2)
+                {
+                    sign2Exists = true;
+                }
+            }
+
+            if (sign1Exists && sign2Exists)
+            {
+                Console.WriteLine($" {sign1} och {sign2} finns med i {userString}.");
+            }
+            else if (sign1Exists)
+            {
+                Console.WriteLine($"Det finns  {sign1} med i {userString}.");
+            }
+            else if (sign2Exists)
+            {
+                Console.WriteLine($"Det finns  {sign2} med i {userString}.");
+            }
+            else
+            {
+                Console.WriteLine($"{sign1} eller ett {sign2} finns med i {userString}.");
+            }
+        }
+    }
+}
